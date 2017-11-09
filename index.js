a8ca646ccf2bcd6954e2509f5e7f7e90aa2a293e
Samuel R. Djiani <djiasara87@yahoo.fr>
2017-11-09 13:06:53 -0500

    update credentials

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
  * Import required packages.
  * Packages should be installed with "npm install".
  */
+require('dotenv').config();
 const express = require('express');
 const aws = require('aws-sdk');
 
@@ -29,7 +30,7 @@ const S3_BUCKET = process.env.S3_BUCKET;
  * Respond to GET requests to /account.
  * Upon request, render the 'account.html' web page in views/ directory.
  */
-app.get('/', (req, res) => res.render('account.html'));
+app.get('/', (req, res) => res.render('index.html'));
 
 /*
  * Respond to GET requests to /sign-s3.
@@ -55,7 +56,8 @@ app.get('/sign-s3', (req, res) => {
     }
     const returnData = {
       signedRequest: data,
-      url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+      url: `https://s3.us-east-2.amazonaws.com/${fileName}`
+      //url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
     };
     res.write(JSON.stringify(returnData));
     res.end();
